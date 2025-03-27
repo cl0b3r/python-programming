eb0afb4f86871acb9d952dc4de51952f1f90613e
Francisco Navarrete <frncisco1243@gmail.com>
2025-03-27 18:53:03 -0300

    updated 01_print

---
+++ 01_basic/01_print.py
@@ -0,0 +1,8 @@
+print("Hello")  # prints to console
+
+print("python", "is", "fun") # automatically adds spaces because of the comas
+
+print("hello","user", sep="-") # sep="" separates the words with the given character "hello-user" 
+
+print("this will print", end="! ") # by default python adds a new line after the print with the end="" we can change that behaviour
+print("this will print too")
--- 01_basic/print.py
+++
@@ -1,1 +0,0 @@
-print("Hello GitHub")  #prints to console
