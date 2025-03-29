ae3af60eb8eb9b49211531bed014fdb9c2110100
Francisco Navarrete <frncisco1243@gmail.com>
2025-03-29 07:47:48 -0300

    data types

---
+++ 01_basic/02_types.py
@@ -0,0 +1,42 @@
+# types --> type() print the type on console print(type(10)) prints int
+# python has a lot of data types
+# int, float, complex, str, bool, NoneType, list, tuple, dict, range, set...
+
+print("int:")
+
+# print(type(10))
+# print(type(0))
+# print(type(-10))
+# print(type(3453463672352))
+
+print("float:")
+# print(type(23.5))
+# print(type(-5.23))
+# print(type(1.0))
+# print(type(1e3)) # --> 1000.0 scientific notation are always float 
+
+
+print("complex:")
+# print(type(1 + 2j))
+
+
+print("str:")
+# print("hello")
+# print(type("")) --> empty also str
+# print(type("1234")) --> if number are inside "" then they are considered str
+# print("""
+# multiline
+# string 
+# """)
+
+
+print("bool") # either true or false
+# print(type(True))
+# print(type(False)) 
+# print(type(10 > 5)) --> logical operations always return a boolean in this case True
+
+
+print("NoneType:")
+# print(type(None)) --> the absence of value 
+
+
